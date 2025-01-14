c8fb94a902fbdf107bca18764dab9b54a6af408b
Dorian Benech <dorian.benech007@gmail.com>
2025-01-14 09:17:19 +0100

    change password

--- src/include/config.php
+++ src/include/config.php
@@ -2,8 +2,8 @@
 // Configuration de la base de données
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'dessin');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'db_etu');
+define('DB_PASS', 'N3twork!');
 
 // Configuration générale
 define('SITE_URL', 'https://vm.dbenech.fr/dessin');
