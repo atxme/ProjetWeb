0df82266f9c1be8f9b8d4d51f2e537d02f4a0228
xruva <audren.vaur@gmail.com>
2025-01-21 16:40:08 +0100

    test

--- src/pages/evaluateur/evaluateur.php
+++ src/pages/evaluateur/evaluateur.php
@@ -222,12 +222,6 @@ try {
                 </div>
                 <!-- Liste des concours -->
                 <table class="profile-table">
-                    <tr>
-                        <th class="label-cell">Nom</th>
-                        <th class="label-cell">Date de début</th>
-                        <th class="label-cell">Date de fin</th>
-                        <th class="label-cell">État</th>
-                    </tr>
                     <?php foreach ($concours as $concour) : ?>
                         <tr class="concours-card">
                             <td class="cell"><?= htmlspecialchars($concour['nom_concours']) ?></td>
